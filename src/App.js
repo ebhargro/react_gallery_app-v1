9897fec3cf78ae277d3bdf261fb134d2dcf28c67
Ebony Hargro <ebonyhargro@intuitdep02569.lan>
2021-05-12 00:54:15 -0700

    Finalizing error routes and importing styling using bootstrap

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@
 // importing all componenets
 import React, { Component } from 'react';
 import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
+import { Container, Jumbotron } from 'react-bootstrap';
 import apiKey from './config';
-import NotFound from './Components/NotFound';
 import Search from './Components/Search';
 import Nav from './Components/Nav';
 import PhotoContainer from './Components/PhotoContainer';
+import NoPage from './Components/NoPage'
 
 
 //Initializing state with app class component
@@ -28,7 +29,7 @@ class App extends Component {
   };
 }
 
-  //Loading external data when our component gets mounted to the DOM via searchTag
+  //Loading external data when our component gets mounted to the DOM via searchTag function
 componentDidMount() {
   this.searchTag();
   this.searchTag('oceans');
@@ -37,7 +38,7 @@ componentDidMount() {
 }
 // Requesting data from the Flickr API using the Fetch API using the searchTag function
 searchTag = (tag) => {
-  fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${tag}&per_page=15&format=json&nojsoncallback=1`)
+  fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${tag}&per_page=16&format=json&nojsoncallback=1`)
   //After the fetch method executes the result is returned in JSON format and then the state is updated by passing in a function that takes the JSON data and returns a new state
     .then(res => res.json())
     .then(response => {
@@ -53,9 +54,14 @@ searchTag = (tag) => {
         }) 
       } else if (tag === 'wolves') {
           this.setState({
-            photos: response.photos.photo,
-            title: tag,
+            wolves: response.photos.photo,
             loading: false
+          })
+        } else {
+          this.setState({
+            photos: response.photos.photo,
+            loading: false,
+            title: tag
           })
         }
         })
@@ -64,38 +70,47 @@ searchTag = (tag) => {
         console.log('Error collecting data', error);
       });
       }
-
+//Adding a loading indicator to aim for Exceeds
 manageLoading = (indicator) => {
   this.setState({loading: indicator});
 }
-//Rendering the child components of our app and passing them external API data state via props
+//Rendering the child components of our app and passing them external API data state via props. Also establishing a header container and welcome message using Bootstrap. Also calling the NoPage component for routes that do not exist
 render () {
   return(
+    <div>
+      <Jumbotron>
+            <Container> 
+              <h1>Welcome to Photo-Finder! 🔍 </h1>
+              <p>
+                This is a simple search app created by Ebony Hargro with the help of Treehouse instructors and community. 
+              </p>
+            </Container>
+      </Jumbotron>
     <BrowserRouter>
-      <Search onSearch={this.performSearch} tag={this.state.title} setLoading={this.manageLoading}/>
-      <Nav/>
-      <Switch>
-        <Route exact path='/' render= { () =>
-         <Redirect to="/oceans"/>} />
-        <Redirect from="/search/oceans" to="/oceans"/>
-        <Route exact path='/oceans' render={ () =>
-         <PhotoContainer data={this.state.oceans} title='Oceans' loading={this.state.loading} onSearch={() => this.performSearch} />} />
+          <Search onSearch={this.searchTag} tag={this.state.title} setLoading={this.manageLoading}/>
+          <Nav/>
+          <Switch>
+            <Route exact path='/' render= { () =>
+            <Redirect to="/oceans"/>} />
+            <Redirect from="/search/oceans" to="/oceans"/>
+            <Route exact path='/oceans' render={ () =>
+            <PhotoContainer data={this.state.oceans} title='Oceans' loading={this.state.loading} onSearch={() => this.searchTag} />} />
 
-        <Redirect from="/search/sunrises" to="/sunrises"/>
-        <Route exact path='/sunrises' render={ () => <PhotoContainer 
-          data={this.state.sunrises} title='Sunrises' loading={this.state.loading} onSearch={ () => this.performSearch}/>} />
+            <Redirect from="/search/sunrises" to="/sunrises"/>
+            <Route exact path='/sunrises' render={ () => <PhotoContainer 
+              data={this.state.sunrises} title='Sunrises' loading={this.state.loading} onSearch={ () => this.searchTag}/>} />
 
-        <Redirect from="/search/wolves" to="/wolves"/>
-          <Route exact path='/wolves' render={ () => <PhotoContainer
-            data={this.state.wolves} title={this.state.title} loading={this.state.loading} onSearch={this.performSearch}/>}
+            <Redirect from="/search/wolves" to="/wolves"/>
+              <Route exact path='/wolves' render={ () => <PhotoContainer
+                data={this.state.wolves} title='Wolves' loading={this.state.loading} onSearch={ () => this.searchTag}/>}
+                />
+            <Route exact path='/search/:tag' render={ () => <PhotoContainer
+              data={this.state.photos} title={this.state.title} loading={this.state.loading} onSearch={ () => this.searchTag}/>}
             />
-        <Route exact path='/search/:tag' render={ () => <PhotoContainer
-          data={this.state.photos} title={this.state.title} loading={this.state.loading} onSearch={this.performSearch}/>}
-        />
-        <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>  
-  )
+            <Route component={NoPage} />
+            </Switch>
+          </BrowserRouter>  
+    </div>)
 }
 }
 
